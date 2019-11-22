ed5c03f0f0801d1f4bff632b9b14e54234930efd
Carlo Costantino <costantinoart@gmail.com>
2019-11-21 18:42:10 -1000

    Wrote fetch block and added book state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,54 @@ import BookList from './bookList/bookList';
 import './App.css';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      showBooks: false,
+      query: 'Science'
+    }
+  }
+
+   componentDidMount() {
+    const query = this.state.query
+    const APIkey = 'AIzaSyBGdKu7x3YcbexK45zPqbHg5MCbtvBkIdM'
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${APIkey}`;
+
+    fetch(url)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later.');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          books: data.items,
+          showBooks: true,
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
   render() {
+    
+    const listOfBooks = this.state.showBooks 
+      ? <BookList books={this.state.books} /> : null
+    
     return (
       <div className="App">
         <header className="App-header">
           <h1>Google Book Search</h1>
         </header>
-        <FilterBox />
-        <BookList />
+        <FilterBox books={this.state.books}/>
+        {listOfBooks}
       </div>
     );
   }
