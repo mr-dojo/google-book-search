a23f2bfb15d4bf5156101692c17dff79cc302b6a
Carlo Costantino <costantinoart@gmail.com>
2019-11-22 10:54:38 -1000

    Merged Search and typeFilter components

--- src/filterBox/filterBox.js
+++ src/filterBox/filterBox.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import Search from './search';
-import TypeFilter from './typeFilter';
 import './filterBox.css';
 
 export default class FilterBox extends Component {
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.handleSearch(e.target.search.value)
+  }
   render() {
     return (
       <section className="filter-box">
-        <Search 
-          handleSearch={this.props.handleSearch}
-        />
-        <TypeFilter />
+        <div className="search-bar">
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <label htmlFor="search">Find Books:</label>
+            <input type="text" name="search" id="search" placeholder="Search Book"></input>
+            <button type="submit">Search</button>
+          </form>
+        </div>
+        <div className="type-filter">
+          <label htmlFor="print">Print Type:</label>
+          <select name="print">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+          </select>
+          <label htmlFor="book">Book Type:</label>
+          <select name="Book Type:">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+          </select>
+        </div>
       </section>
     )
   }
